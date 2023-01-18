22b300d95d85ffb67644d1b895a186c0ca548e85
Margarita Glushkova <93099190+RitaGlushkova@users.noreply.github.com>
2023-01-18 11:22:49 +0000

    Update raftapi.go

--- raftapi.go
+++ raftapi.go
@@ -11,6 +11,7 @@ import (
 	"google.golang.org/grpc"
 )
 
+//Hello
 // These are calls from the Raft engine that we need to send out over gRPC.
 
 type raftAPI struct {
@@ -73,6 +74,7 @@ func (r raftAPI) AppendEntries(id raft.ServerID, target raft.ServerAddress, args
 
 // RequestVote sends the appropriate RPC to the target node.
 func (r raftAPI) RequestVote(id raft.ServerID, target raft.ServerAddress, args *raft.RequestVoteRequest, resp *raft.RequestVoteResponse) error {
+
 	c, err := r.getPeer(id, target)
 	if err != nil {
 		return err
